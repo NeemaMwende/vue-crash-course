578e23c81d0f9ca869b84a495314543bf31d50a3
Neema Mwende <gracepete12@gmail.com>
2025-03-29 02:16:42 +0300

    use of  v-html

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h6 v-html="rawHtml"></h6>
+    <p>Using text interpolation: {{ rawHtml }}</p>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      rawHtml:
+        '<h1>This is to see how v-html works</h1> <span style="color: green">This should be green.</span>',
+    }
+  },
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
+    /* display: flex; */
     align-items: center;
   }
 }
